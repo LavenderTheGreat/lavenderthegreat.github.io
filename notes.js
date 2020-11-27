5d33f76cf2c0dfd352786fdd009c8d8db674e5b3
LavenderTheGreat <dach.soa@gmail.com>
2020-11-27 21:43:53 +0000

    Add frame for a blog or something!

---
+++ notes.js
@@ -0,0 +1,41 @@
+function makeRequest(method, url) {
+    console.log("Requesting " + url)
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            console.log("Retrieved " + url)
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+async function loadFile(name){
+
+    var converter = new showdown.Converter(),
+        htmlText      = converter.makeHtml(await makeRequest("GET", name));
+    
+    console.log(htmlText)
+
+    document.getElementById("article").innerHTML = htmlText
+
+}
+
+const url = new URL(window.location.href)
+let args = url.searchParams
+
+loadFile("./md/" + args.get("a") + ".md")
